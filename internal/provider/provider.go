29a323118246f2e94069295f736a626fd9b810df
Kyle Carberry <kyle@carberry.com>
2022-08-09 16:26:47 +0000

    feat: Add `coder_host` resource for attributes from the host
    Our [Docker template](https://github.com/coder/coder/blob/main/examples/templates/docker/main.tf#L16-L60) exposes variables
    which are easily obtainable from the host. By adding this resource, we allow simple setups that depend on host
    architecture to work without input from the user.

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -7,6 +7,7 @@ import (
 	"net/url"
 	"os"
 	"reflect"
+	"runtime"
 	"strings"
 
 	"github.com/google/uuid"
@@ -147,6 +148,28 @@ func New() *schema.Provider {
 						Type:        schema.TypeString,
 						Computed:    true,
 						Description: "Name of the workspace.",
+					},
+				},
+			},
+			"coder_host": {
+				Description: "Use this data source to get information about the host.",
+				ReadContext: func(c context.Context, rd *schema.ResourceData, i interface{}) diag.Diagnostics {
+					rd.SetId(uuid.NewString())
+					rd.Set("os", runtime.GOOS)
+					rd.Set("arch", runtime.GOARCH)
+
+					return nil
+				},
+				Schema: map[string]*schema.Schema{
+					"os": {
+						Type:        schema.TypeString,
+						Computed:    true,
+						Description: "The operating system of the host.",
+					},
+					"arch": {
+						Type:        schema.TypeString,
+						Computed:    true,
+						Description: "The architecture of the host.",
 					},
 				},
 			},
