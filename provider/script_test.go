5ecd935c2e9cd79ebe82fbf8626236c080af01f4
Kyle Carberry <kyle@carberry.com>
2023-09-25 22:45:14 +0000

    fix: validate that `coder_script` has a way to run
    Previously, this would just silently push the script and
    it would never run, because it wasn't configured to do so.
    
    This changes it so all scripts have to be ran at some point.

---
+++ provider/script_test.go
@@ -0,0 +1,75 @@
+package provider_test
+
+import (
+	"regexp"
+	"testing"
+
+	"github.com/coder/terraform-provider-coder/provider"
+	"github.com/stretchr/testify/require"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+)
+
+func TestScript(t *testing.T) {
+	t.Parallel()
+
+	resource.Test(t, resource.TestCase{
+		Providers: map[string]*schema.Provider{
+			"coder": provider.New(),
+		},
+		IsUnitTest: true,
+		Steps: []resource.TestStep{{
+			Config: `
+			provider "coder" {
+			}
+			resource "coder_script" "example" {
+				agent_id = "some id"
+				display_name = "Hey"
+				script = "Wow"
+				cron = "* * * * *"
+			}
+			`,
+			Check: func(state *terraform.State) error {
+				require.Len(t, state.Modules, 1)
+				require.Len(t, state.Modules[0].Resources, 1)
+				script := state.Modules[0].Resources["coder_script.example"]
+				require.NotNil(t, script)
+				t.Logf("script attributes: %#v", script.Primary.Attributes)
+				for key, expected := range map[string]string{
+					"agent_id":     "some id",
+					"display_name": "Hey",
+					"script":       "Wow",
+					"cron":         "* * * * *",
+				} {
+					require.Equal(t, expected, script.Primary.Attributes[key])
+				}
+				return nil
+			},
+		}},
+	})
+}
+
+func TestScriptNeverRuns(t *testing.T) {
+	t.Parallel()
+
+	resource.Test(t, resource.TestCase{
+		Providers: map[string]*schema.Provider{
+			"coder": provider.New(),
+		},
+		IsUnitTest: true,
+		Steps: []resource.TestStep{{
+			Config: `
+			provider "coder" {
+			}
+			resource "coder_script" "example" {
+				agent_id = ""
+				display_name = "Hey"
+				script = "Wow"
+			}
+			`,
+			ExpectError: regexp.MustCompile(`at least one of run_on_start, run_on_stop, or cron must be set`),
+		}},
+	})
+}
