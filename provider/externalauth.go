c31c582dc779cd98e72674737792a3ed3012ac1c
Kyle Carberry <kyle@carberry.com>
2023-10-03 10:25:15 -0500

    feat: add `coder_external_auth` and deprecate `coder_git_auth` (#163)
    * feat: add `coder_external_auth` and deprecate `coder_git_auth`
    
    * Improve docs

---
+++ provider/externalauth.go
@@ -0,0 +1,44 @@
+package provider
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+// externalAuthDataSource returns a schema for an external authentication data source.
+func externalAuthDataSource() *schema.Resource {
+	return &schema.Resource{
+		Description: "Use this data source to require users to authenticate with an external service prior to workspace creation. This can be used to pre-authenticate external services in a workspace. (e.g. gcloud, gh, docker, etc)",
+		ReadContext: func(ctx context.Context, rd *schema.ResourceData, i interface{}) diag.Diagnostics {
+			id, ok := rd.Get("id").(string)
+			if !ok || id == "" {
+				return diag.Errorf("id is required")
+			}
+			rd.SetId(id)
+
+			accessToken := os.Getenv(ExternalAuthAccessTokenEnvironmentVariable(id))
+			rd.Set("access_token", accessToken)
+			return nil
+		},
+		Schema: map[string]*schema.Schema{
+			"id": {
+				Type:        schema.TypeString,
+				Description: "The ID of a configured external auth provider set up in your Coder deployment.",
+				Required:    true,
+			},
+			"access_token": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "The access token returned by the external auth provider. This can be used to pre-authenticate command-line tools.",
+			},
+		},
+	}
+}
+
+func ExternalAuthAccessTokenEnvironmentVariable(id string) string {
+	return fmt.Sprintf("CODER_EXTERNAL_AUTH_ACCESS_TOKEN_%s", id)
+}
--- provider/gitauth.go
+++ provider/gitauth.go
@@ -12,7 +12,8 @@ import (
 // gitAuthDataSource returns a schema for a Git authentication data source.
 func gitAuthDataSource() *schema.Resource {
 	return &schema.Resource{
-		Description: "Use this data source to require users to authenticate with a Git provider prior to workspace creation. This can be used to perform an authenticated `git clone` in startup scripts.",
+		DeprecationMessage: "Use the `coder_external_auth` data source instead.",
+		Description:        "Use this data source to require users to authenticate with a Git provider prior to workspace creation. This can be used to perform an authenticated `git clone` in startup scripts.",
 		ReadContext: func(ctx context.Context, rd *schema.ResourceData, i interface{}) diag.Diagnostics {
 			rawID, ok := rd.GetOk("id")
 			if !ok {
