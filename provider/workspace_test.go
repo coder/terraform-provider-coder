7b1926ca5ca7270545923164cc812eab3d08e427
Garrett Delfosse <garrett@coder.com>
2024-04-02 22:59:45 +0000

    use raw string

--- provider/workspace_test.go
+++ provider/workspace_test.go
@@ -16,7 +16,7 @@ func TestWorkspace(t *testing.T) {
 	t.Setenv("CODER_WORKSPACE_OWNER_NAME", "Mr Owner")
 	t.Setenv("CODER_WORKSPACE_OWNER_EMAIL", "owner123@example.com")
 	t.Setenv("CODER_WORKSPACE_OWNER_SESSION_TOKEN", "abc123")
-	t.Setenv("CODER_WORKSPACE_OWNER_GROUPS", "[\"group1\",\"group2\"]")
+	t.Setenv("CODER_WORKSPACE_OWNER_GROUPS", `["group1", "group2"]`)
 	t.Setenv("CODER_WORKSPACE_TEMPLATE_ID", "templateID")
 	t.Setenv("CODER_WORKSPACE_TEMPLATE_NAME", "template123")
 	t.Setenv("CODER_WORKSPACE_TEMPLATE_VERSION", "v1.2.3")
