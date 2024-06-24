3a98b58efe3edcd5a6fbe2a346cc64ca9e98121f
Stephen Kirby <me@skirby.dev>
2024-06-24 20:28:08 +0000

    attempting data source testing

--- provider/examples_test.go
+++ provider/examples_test.go
@@ -14,6 +14,7 @@ import (
 func TestExamples(t *testing.T) {
 	t.Parallel()
 
+	// Resource examples
 	for _, testDir := range []string{
 		"coder_workspace_tags",
 	} {
@@ -28,6 +29,26 @@ func TestExamples(t *testing.T) {
 				IsUnitTest: true,
 				Steps: []resource.TestStep{{
 					Config: mustReadFile(t, "../examples/resources/"+testDir+"/resource.tf"),
+				}},
+			})
+		})
+	}
+
+	// Data source examples
+	for _, testDir := range []string{
+		"coder_parameter",
+	} {
+		t.Run(testDir, func(t *testing.T) {
+			testDir := testDir
+			t.Parallel()
+
+			resource.Test(t, resource.TestCase{
+				Providers: map[string]*schema.Provider{
+					"coder": provider.New(),
+				},
+				IsUnitTest: true,
+				Steps: []resource.TestStep{{
+					Config: mustReadFile(t, "../examples/data-sources/"+testDir+"/data-source.tf"),
 				}},
 			})
 		})
