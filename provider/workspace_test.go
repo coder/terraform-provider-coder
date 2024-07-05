7ed088d0942fc4b24cf26c3e031a15618667f3c0
Muhammad Atif Ali <atif@coder.com>
2024-07-05 11:40:47 +0300

    chore!: remove deprecated items from `coder_workspace` data source (#255)

--- provider/workspace_test.go
+++ provider/workspace_test.go
@@ -14,13 +14,6 @@ import (
 )
 
 func TestWorkspace(t *testing.T) {
-	t.Setenv("CODER_WORKSPACE_OWNER", "owner123")
-	t.Setenv("CODER_WORKSPACE_OWNER_ID", "11111111-1111-1111-1111-111111111111")
-	t.Setenv("CODER_WORKSPACE_OWNER_NAME", "Mr Owner")
-	t.Setenv("CODER_WORKSPACE_OWNER_EMAIL", "owner123@example.com")
-	t.Setenv("CODER_WORKSPACE_OWNER_SESSION_TOKEN", "abc123")
-	t.Setenv("CODER_WORKSPACE_OWNER_GROUPS", `["group1", "group2"]`)
-	t.Setenv("CODER_WORKSPACE_OWNER_OIDC_ACCESS_TOKEN", "supersecret")
 	t.Setenv("CODER_WORKSPACE_TEMPLATE_ID", "templateID")
 	t.Setenv("CODER_WORKSPACE_TEMPLATE_NAME", "template123")
 	t.Setenv("CODER_WORKSPACE_TEMPLATE_VERSION", "v1.2.3")
@@ -49,16 +42,9 @@ func TestWorkspace(t *testing.T) {
 				t.Log(value)
 				assert.Equal(t, "https://example.com:8080", attribs["access_url"])
 				assert.Equal(t, "8080", attribs["access_port"])
-				assert.Equal(t, "owner123", attribs["owner"])
-				assert.Equal(t, "11111111-1111-1111-1111-111111111111", attribs["owner_id"])
-				assert.Equal(t, "Mr Owner", attribs["owner_name"])
-				assert.Equal(t, "owner123@example.com", attribs["owner_email"])
-				assert.Equal(t, "group1", attribs["owner_groups.0"])
-				assert.Equal(t, "group2", attribs["owner_groups.1"])
 				assert.Equal(t, "templateID", attribs["template_id"])
 				assert.Equal(t, "template123", attribs["template_name"])
 				assert.Equal(t, "v1.2.3", attribs["template_version"])
-				assert.Equal(t, "supersecret", attribs["owner_oidc_access_token"])
 				return nil
 			},
 		}},
@@ -66,9 +52,6 @@ func TestWorkspace(t *testing.T) {
 }
 
 func TestWorkspace_UndefinedOwner(t *testing.T) {
-	t.Setenv("CODER_WORKSPACE_OWNER", "owner123")
-	t.Setenv("CODER_WORKSPACE_OWNER_SESSION_TOKEN", "abc123")
-	t.Setenv("CODER_WORKSPACE_OWNER_GROUPS", `["group1", "group2"]`)
 	t.Setenv("CODER_WORKSPACE_TEMPLATE_ID", "templateID")
 	t.Setenv("CODER_WORKSPACE_TEMPLATE_NAME", "template123")
 	t.Setenv("CODER_WORKSPACE_TEMPLATE_VERSION", "v1.2.3")
@@ -95,8 +78,9 @@ func TestWorkspace_UndefinedOwner(t *testing.T) {
 				value := attribs["transition"]
 				require.NotNil(t, value)
 				t.Log(value)
-				assert.Equal(t, "owner123", attribs["owner"])
-				assert.Equal(t, "default@example.com", attribs["owner_email"])
+				assert.Equal(t, "templateID", attribs["template_id"])
+				assert.Equal(t, "template123", attribs["template_name"])
+				assert.Equal(t, "v1.2.3", attribs["template_version"])
 				// Skip other asserts
 				return nil
 			},
@@ -107,13 +91,6 @@ func TestWorkspace_UndefinedOwner(t *testing.T) {
 func TestWorkspace_MissingTemplateName(t *testing.T) {
 	t.Setenv("CODER_WORKSPACE_BUILD_ID", "1") // Let's pretend this is a workspace build
 
-	t.Setenv("CODER_WORKSPACE_OWNER", "owner123")
-	t.Setenv("CODER_WORKSPACE_OWNER_ID", "11111111-1111-1111-1111-111111111111")
-	t.Setenv("CODER_WORKSPACE_OWNER_NAME", "Mr Owner")
-	t.Setenv("CODER_WORKSPACE_OWNER_EMAIL", "owner123@example.com")
-	t.Setenv("CODER_WORKSPACE_OWNER_SESSION_TOKEN", "abc123")
-	t.Setenv("CODER_WORKSPACE_OWNER_GROUPS", `["group1", "group2"]`)
-	t.Setenv("CODER_WORKSPACE_OWNER_OIDC_ACCESS_TOKEN", "supersecret")
 	t.Setenv("CODER_WORKSPACE_TEMPLATE_ID", "templateID")
 	// CODER_WORKSPACE_TEMPLATE_NAME is missing
 	t.Setenv("CODER_WORKSPACE_TEMPLATE_VERSION", "v1.2.3")
