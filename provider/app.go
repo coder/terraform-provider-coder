3ddf21c6430b233586dfad90212a6c51302a2978
Danielle Maywood <danielle@coder.com>
2024-09-09 11:39:40 +0000

    docs: document minimum version required for 'hidden' attribute

--- provider/app.go
+++ provider/app.go
@@ -218,7 +218,7 @@ func appResource() *schema.Resource {
 			},
 			"hidden": {
 				Type:        schema.TypeBool,
-				Description: "Determines if the app is visible in the UI.",
+				Description: "Determines if the app is visible in the UI (minimum coder version: v2.16).",
 				Default:     false,
 				ForceNew:    true,
 				Optional:    true,
