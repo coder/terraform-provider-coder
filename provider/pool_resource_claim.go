7f0dab99d59d541c566a217c2aa9526a28d196f5
Danny Kopping <danny@coder.com>
2024-12-11 11:16:29 +0200

    Inject instance IDs in by pool name
    Signed-off-by: Danny Kopping <danny@coder.com>

--- provider/pool_resource_claim.go
+++ provider/pool_resource_claim.go
@@ -2,7 +2,11 @@ package provider
 
 import (
 	"context"
+	"crypto/sha1"
+	"encoding/base32"
+	"fmt"
 
+	"github.com/coder/terraform-provider-coder/provider/helpers"
 	"github.com/google/uuid"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -15,22 +19,28 @@ func poolResourceClaimResource() *schema.Resource {
 		Description: "Use this resource to claim a resource pool entry.",
 		CreateContext: func(_ context.Context, resourceData *schema.ResourceData, i interface{}) diag.Diagnostics {
 			resourceData.SetId(uuid.NewString())
+
+			pool := resourceData.Get("pool_name").(string)
+
+			// SHA-1 because Base32/64 produces an invalid env infix
+			poolName := sha1.Sum([]byte(pool))
+			key := fmt.Sprintf("CODER_RESOURCEPOOL_%x_ENTRY_INSTANCE_ID", poolName)
+			instanceId, err := helpers.RequireEnv(key)
+			if err != nil {
+				return diag.Errorf("failed to get instance ID: %s", err.Error())
+			}
+			decoded, err := base32.StdEncoding.DecodeString(instanceId)
+			if err != nil {
+				return diag.Errorf("failed to decode instance ID %q: %s", instanceId, err.Error())
+			}
+			_ = resourceData.Set("instance_id", string(decoded))
+
 			return nil
 		},
-		ReadContext: func(ctx context.Context, resourceData *schema.ResourceData, i interface{}) diag.Diagnostics {
-			// poolb64 := base64.StdEncoding.EncodeToString([]byte(resourceData.Get("pool_name").(string)))
-			// key := fmt.Sprintf("CODER_RESOURCE_POOL_%s_ENTRY_OBJECT_ID", poolb64)
-			// objectId, err := helpers.RequireEnv(key)
-			// if err != nil {
-			// 	return diag.Errorf("resource pool entry object ID not found in env %q: %s", key, err)
-			// }
-			//
-			// if err = resourceData.Set("object_id", objectId); err != nil {
-			// 	return diag.Errorf("failed to set resource pool entry object ID: %s", err)
-			// }
+		ReadContext: func(ctx context.Context, data *schema.ResourceData, i interface{}) diag.Diagnostics {
 			return nil
 		},
-		DeleteContext: func(ctx context.Context, resourceData *schema.ResourceData, i interface{}) diag.Diagnostics {
+		DeleteContext: func(ctx context.Context, data *schema.ResourceData, i interface{}) diag.Diagnostics {
 			return nil
 		},
 		Schema: map[string]*schema.Schema{
@@ -40,10 +50,10 @@ func poolResourceClaimResource() *schema.Resource {
 				ForceNew:    true,
 				Description: "The name of the pool from which an entry will be claimed.",
 			},
-			"object_id": {
+			"instance_id": {
 				Type:        schema.TypeString,
 				Computed:    true,
-				Description: "The object ID of the pooled entry resource",
+				Description: "The instance ID of the pooled entry resource",
 			},
 		},
 	}
@@ -88,9 +98,20 @@ func poolResourceClaimableResource() *schema.Resource {
 				},
 			},
 			"other": {
-				Type:     schema.TypeString,
+				Type:     schema.TypeSet,
 				ForceNew: true,
 				Optional: true,
+
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"instance_id": {
+							Type:        schema.TypeString,
+							Description: "The ID of the instance.",
+							ForceNew:    true,
+							Required:    true,
+						},
+					},
+				},
 			},
 		},
 	}
