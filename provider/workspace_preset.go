2f0054689a2a0e6e684ce0f30f6dbf0adeb58ec3
Sas Swart <sas.swart.cdk@gmail.com>
2025-01-31 14:18:17 +0000

    add validation and tests for coder_workspace_presets

--- provider/workspace_preset.go
+++ provider/workspace_preset.go
@@ -3,9 +3,9 @@ package provider
 import (
 	"context"
 
-	"github.com/google/uuid"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/mitchellh/mapstructure"
 )
 
 type WorkspacePreset struct {
@@ -17,13 +17,38 @@ func workspacePresetDataSource() *schema.Resource {
 	return &schema.Resource{
 		SchemaVersion: 1,
 
-		Description: "",
+		Description: "Use this data source to predefine common configurations for workspaces.",
 		ReadContext: func(ctx context.Context, rd *schema.ResourceData, i interface{}) diag.Diagnostics {
-			rd.SetId(uuid.NewString())
+			var preset WorkspacePreset
+			err := mapstructure.Decode(struct {
+				Name       interface{}
+				Parameters interface{}
+			}{
+				Name:       rd.Get("name"),
+				Parameters: rd.Get("parameters"),
+			}, &preset)
+			if err != nil {
+				return diag.Errorf("decode workspace preset: %s", err)
+			}
+
+			if preset.Name == "" {
+				return diag.Errorf("workspace preset name must be set")
+			}
+
+			if len(preset.Parameters) == 0 {
+				return diag.Errorf("workspace preset must define a value for at least one parameter")
+			}
+
+			rd.SetId(preset.Name)
 
 			return nil
 		},
 		Schema: map[string]*schema.Schema{
+			"id": {
+				Type:        schema.TypeString,
+				Description: "ID of the workspace preset.",
+				Computed:    true,
+			},
 			"name": {
 				Type:        schema.TypeString,
 				Description: "Name of the workspace preset.",
--- provider/workspace_preset_test.go
+++ provider/workspace_preset_test.go
@@ -1,6 +1,7 @@
 package provider_test
 
 import (
+	"regexp"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
@@ -9,6 +10,7 @@ import (
 )
 
 func TestWorkspacePreset(t *testing.T) {
+	// Happy Path:
 	resource.Test(t, resource.TestCase{
 		ProviderFactories: coderFactory(),
 		IsUnitTest:        true,
@@ -30,6 +32,96 @@ func TestWorkspacePreset(t *testing.T) {
 				require.Equal(t, attrs["parameters.region"], "us-east1-a")
 				return nil
 			},
+		}},
+	})
+
+	// Given the Name field is not provided
+	resource.Test(t, resource.TestCase{
+		ProviderFactories: coderFactory(),
+		IsUnitTest:        true,
+		Steps: []resource.TestStep{{
+			Config: `
+			data "coder_workspace_preset" "preset_1" {
+				parameters = {
+					"region" = "us-east1-a"
+				}
+			}`,
+			// This is from terraform's validation based on our schema, not based on our validation in ReadContext:
+			ExpectError: regexp.MustCompile("The argument \"name\" is required, but no definition was found"),
+		}},
+	})
+
+	// Given the Name field is empty
+	resource.Test(t, resource.TestCase{
+		ProviderFactories: coderFactory(),
+		IsUnitTest:        true,
+		Steps: []resource.TestStep{{
+			Config: `
+			data "coder_workspace_preset" "preset_1" {
+				name = ""
+				parameters = {
+					"region" = "us-east1-a"
+				}
+			}`,
+			ExpectError: regexp.MustCompile("workspace preset name must be set"),
+		}},
+	})
+
+	// Given the Name field is not a string
+	resource.Test(t, resource.TestCase{
+		ProviderFactories: coderFactory(),
+		IsUnitTest:        true,
+		Steps: []resource.TestStep{{
+			Config: `
+			data "coder_workspace_preset" "preset_1" {
+				name = [1, 2, 3]
+				parameters = {
+					"region" = "us-east1-a"
+				}
+			}`,
+			ExpectError: regexp.MustCompile("Incorrect attribute value type"),
+		}},
+	})
+
+	// Given the Parameters field is not provided
+	resource.Test(t, resource.TestCase{
+		ProviderFactories: coderFactory(),
+		IsUnitTest:        true,
+		Steps: []resource.TestStep{{
+			Config: `
+			data "coder_workspace_preset" "preset_1" {
+				name = "preset_1"
+			}`,
+			ExpectError: regexp.MustCompile("The argument \"parameters\" is required, but no definition was found"),
+		}},
+	})
+
+	// Given the Parameters field is empty
+	resource.Test(t, resource.TestCase{
+		ProviderFactories: coderFactory(),
+		IsUnitTest:        true,
+		Steps: []resource.TestStep{{
+			Config: `
+			data "coder_workspace_preset" "preset_1" {
+				name = "preset_1"
+				parameters = {}
+			}`,
+			ExpectError: regexp.MustCompile("workspace preset must define a value for at least one parameter"),
+		}},
+	})
+
+	// Given the Parameters field is not a map
+	resource.Test(t, resource.TestCase{
+		ProviderFactories: coderFactory(),
+		IsUnitTest:        true,
+		Steps: []resource.TestStep{{
+			Config: `
+			data "coder_workspace_preset" "preset_1" {
+				name = "preset_1"
+				parameters = "not a map"
+			}`,
+			// This is from terraform's validation based on our schema, not based on our validation in ReadContext:
+			ExpectError: regexp.MustCompile("Inappropriate value for attribute \"parameters\": map of string required"),
 		}},
 	})
 }
