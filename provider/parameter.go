16ab5102775ebad5f76de87524496ce516d8f5ca
Steven Masley <stevenmasley@gmail.com>
2025-03-17 10:24:24 -0500

    move code block

--- provider/parameter.go
+++ provider/parameter.go
@@ -250,6 +250,12 @@ func parameterDataSource() *schema.Resource {
 				ValidateFunc: validation.StringInSlice(toStrings(ParameterFormTypes()), false),
 				Description:  fmt.Sprintf("The type of this parameter. Must be one of: [%s].", strings.Join(toStrings(ParameterFormTypes()), ", ")),
 			},
+			"form_type_metadata": {
+				Type:        schema.TypeString,
+				Default:     `{}`,
+				Description: "JSON encoded string containing the metadata for controlling the appearance of this parameter in the UI.",
+				Optional:    true,
+			},
 			"mutable": {
 				Type:        schema.TypeBool,
 				Optional:    true,
@@ -275,12 +281,6 @@ func parameterDataSource() *schema.Resource {
 					}
 					return nil, nil
 				},
-			},
-			"form_type_metadata": {
-				Type:        schema.TypeString,
-				Default:     `{}`,
-				Description: "JSON encoded string containing the metadata for controlling the appearance of this parameter in the UI.",
-				Optional:    true,
 			},
 			"option": {
 				Type:        schema.TypeList,
