89519404c9a15735fc4984d97188ecb403377916
Steven Masley <stevenmasley@gmail.com>
2025-04-07 14:11:46 -0500

    adding the example before release
    The example needs the release. So it's invalid until we push a
    tag

--- examples/data-sources/coder_parameter/data-source.tf
+++ examples/data-sources/coder_parameter/data-source.tf
@@ -115,35 +115,3 @@ data "coder_parameter" "home_volume_size" {
     monotonic = "increasing"
   }
 }
-
-data "coder_parameter" "tools" {
-  name        = "Tools"
-  description = "What tools do you want to install?"
-  type        = "list(string)"
-  form_type   = "multi-select"
-  styling = jsonencode({
-    # Arbitrary JSON object to be passed to the frontend
-  })
-  default = jsonencode(["git", "docker"])
-
-  option {
-    value = "Docker"
-    name  = "docker"
-    icon  = "/icon/docker.svg"
-  }
-  option {
-    value = "Git"
-    name  = "git"
-    icon  = "/icon/git.svg"
-  }
-  option {
-    value = "Golang"
-    name  = "go"
-    icon  = "/icon/go.svg"
-  }
-  option {
-    value = "Typescript"
-    name  = "ts"
-    icon  = "/icon/typescript.svg"
-  }
-}
