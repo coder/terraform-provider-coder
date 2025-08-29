22a6d6ecf1af11639a654cdef2555be557e78d3e
Rafael Rodriguez <rafael@coder.com>
2025-08-29 16:17:43 -0500

    remove duplicate test

--- provider/app_test.go
+++ provider/app_test.go
@@ -556,15 +556,6 @@ func TestApp(t *testing.T) {
 					"#install-coder-desktop) to use this button.",
 			},
 			{
-				name: "TooltipMaxLength", // < 512 characters
-				tooltip: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut" +
-					"labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco" +
-					"laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in" +
-					"voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat" +
-					"non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut" +
-					"perspiciatis unde omnis iste natus error sit voluptatem accusant",
-			},
-			{
 				name: "TooltipTooLong", // > 512 characters
 				tooltip: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor " +
 					"incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud " +
