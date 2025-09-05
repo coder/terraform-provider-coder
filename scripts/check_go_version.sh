bfceb4cf7de3e805a06d3485d20cf119f3b9fbbc
Cian Johnston <cian@coder.com>
2025-09-05 11:50:36 +0100

    add --fix

--- scripts/check_go_version.sh
+++ scripts/check_go_version.sh
@@ -1,11 +1,37 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+fix=0
+args="$(getopt -o "" -l fix -- "$@")"
+eval set -- "$args"
+while true; do
+	case "$1" in
+	--fix)
+		fix=1
+        shift
+		;;
+	--)
+		shift
+		break
+		;;
+	*)
+		error "Unrecognized option: $1"
+		;;
+	esac
+done
+
 MOD_VERSION=$(go mod edit -json | jq -r .Go)
 echo "go.mod version: $MOD_VERSION"
 STATUS=0
 
-for wf in .github/workflows/*.yml; do
+if [[ $fix -eq 1 ]]; then
+    for wf in .github/workflows/*.{yml,yaml}; do
+        sed -i "s/go-version:.*/go-version: \"${MOD_VERSION}\"/g" "${wf}"
+    done
+    exit $STATUS
+fi
+
+for wf in .github/workflows/*.{yml,yaml}; do
     WF_VERSIONS=$(yq -r '.jobs[].steps[] | select(.with["go-version"]) | .with["go-version"]' -o=tsv "$wf" | grep -v '^---$' || true)
     if [[ -z "$WF_VERSIONS" ]]; then
         continue
@@ -20,5 +46,9 @@ for wf in .github/workflows/*.yml; do
         fi
     done
 done
+
+if [[ $STATUS  -eq 1 ]]; then
+    echo "Re-run this script with --fix to automatically update workflows to match go.mod"
+fi
 
 exit $STATUS
